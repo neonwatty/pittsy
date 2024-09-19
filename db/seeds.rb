cb5e6325015dc9cf8a236d0120005224a3505754
Jeremy Watt <jermwatt@gmail.com>
2024-09-19 07:37:31 -0700

    working on user role controls

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,46 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-user = User.where(email: "neonwatty@gmail.com").first_or_initialize
+user = User.where(email: "rob@gmail.com").first_or_initialize
 user.update!(
   password: "password",
   password_confirmation: "password",
   role: "admin",
-  first_name: "Jeremy",
+  first_name: "Robert",
   last_name: "Watt",
   status: 0,
-  avatar: Rails.root.join("app/assets/images/avatars/avatar_1.jpg").open
+  avatar: Rails.root.join("app/assets/images/avatars/avatar_1.png").open
 )
 
-user = User.where(email: "jermwatt@gmail.com").first_or_initialize
+user = User.where(email: "ben@gmail.com").first_or_initialize
 user.update!(
   password: "password",
   password_confirmation: "password",
   role: "employee",
-  first_name: "Rubber",
-  last_name: "Ducky",
-  status: 0,
-  avatar: Rails.root.join("app/assets/images/avatars/avatar_2.jpg").open
+  first_name: "Ben",
+  last_name: "Schmidtetter",
+  status: 1,
+  avatar: Rails.root.join("app/assets/images/avatars/avatar_2.png").open
+)
+
+user = User.where(email: "josh@gmail.com").first_or_initialize
+user.update!(
+  password: "password",
+  password_confirmation: "password",
+  role: "employee",
+  first_name: "Josh",
+  last_name: "Schmidtetter",
+  status: 1,
+  avatar: Rails.root.join("app/assets/images/avatars/avatar_3.png").open
+)
+
+user = User.where(email: "bob@gmail.com").first_or_initialize
+user.update!(
+  password: "password",
+  password_confirmation: "password",
+  role: "employee",
+  first_name: "Bob",
+  last_name: "Androsik",
+  status: 1,
+  avatar: Rails.root.join("app/assets/images/avatars/avatar_4.png").open
 )
