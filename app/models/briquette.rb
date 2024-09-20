62500209984682ad37361f266fa7f927079901aa
Jeremy Watt <jermwatt@gmail.com>
2024-09-20 10:08:16 -0700

    starting briquettes join

--- app/models/briquette.rb
+++ app/models/briquette.rb
@@ -1,8 +1,6 @@
 class Briquette < ApplicationRecord
   belongs_to :shift
 
-  validates :measurement_time, presence: true
-  validates :briquette_speed, presence: true
   validates :status, presence: true, inclusion: { in: %w[inactive active complete] }
 
   private
